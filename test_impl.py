5a4e223628e3239f1b14abfd939ca9319c2535b9
Florian Katerndahl <florian@katerndahl.com>
2025-07-02 00:25:29 +0200

    remove test_imply.py

--- test_impl.py
+++
@@ -1,8 +0,0 @@
-from colorful_gliders.PointCloud import PointCloud
-
-pc = PointCloud()
-pc.load_from_laz("data/chair-final.laz")
-pc.write_to_laz("data/out.laz")
-
-pc.construct_mesh()
-pc.write_mesh("data/model.ply")
